005ccf60cf97d9705a174a0c53a2ae8c4550a7a0
Steve Klise <sk@skli.se>
2014-06-04 22:44:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var Cylon = require('cylon');
-var io = require('socket.io-client');
-
-socket = io.connect('http://sk-empire-socket.herokuapp.com');
+var socket = require('socket.io-client')('http://sk-empire-socket.herokuapp.com')
 
 Cylon.robot({
   connection: { name: 'beaglebone', adaptor: 'beaglebone' },
@@ -9,15 +7,9 @@ Cylon.robot({
 
   work: function(my) {
     my.led.turnOff()
-    //socket.on('connect', function () { my.led.turnOn});
-    //every((1).second(), my.led.toggle);
     socket.on('flash', function () { 
-        my.led.turnOn(); 
-        //after(250, my.led.turnOff)
-        setTimeout(function () {
-            console.log('timedout')
-            my.led.turnOff()
-        }, 150);
+      my.led.turnOn();
+      after(250, my.led.turnOff)
     }); 
   }
 }).start();
